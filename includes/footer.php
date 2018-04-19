9b47b2305a5f8658816f1a822fdf937e15ce3f39
BOS <38324151+devopbos@users.noreply.github.com>
2018-04-19 12:29:11 -0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+
+
+        <footer class="footer-pad" "app-footer">
+
+        </footer>
+
+    </div>
+    <script src="js/app.js" charset="utf-8"></script>
+    <script src="particles.js" charset="utf-8"></script>
+    <script src="particles.min.js" charset="utf-8"></script>
+    <script src="scripts/payload.js" charset="utf-8"></script>
+    <script type="text/javascript">
+        // # inner script 2
+
+    </script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+?>
+<?php require "includes/header.php";?>
+
+<section id="home" class="home-panel">
+
+    <div class="presentation-point">
+        <div id="particles-js">
+
+        </div>
+        <div class="deep-screen"></div>
+        <div class="static-converter" style="height: inherit;">
+            <div class="box-non-equal">
+                <div class="left-box">
+                    <div class="target-text fadeInUp">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit sed do eiusmod
+                    </div>
+                    <div class="info-text">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ali
+                    </div>
+                    <div class="sub-inputs-form-fields">
+                        <form class="" action="index.html" method="post">
+                            <div class="splited">
+                                <div class="input-group">
+                                    <label for="">name</label>
+                                    <input type="text" name="" value="">
+                                </div>
+                                <div class="input-group">
+                                    <label for="">email</label>
+                                    <input type="email" name="" value="">
+                                </div>
+                            </div>
+                            <button type="button" name="button"> recieve updates <i class="arrow-right"></i> </button>
+                        </form>
+                    </div>
+                </div>
+                <div class="right-box">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="about" id="about">
+        <div class="static-converter">
+            <div class="box-non-equal-2">
+                <div class="box-1">
+                    <img src="resources/trading.jpg" width="100%" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<?php require "includes/footer.php";?>
